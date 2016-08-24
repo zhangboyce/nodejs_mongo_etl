e59a2169d4a5e4117ec4fc583e4735c9bfae1849
Boyce <Boyce.zhang@ccegroup.com>
2016-08-24 18:34:53 +0800

    bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = {
-  RssFromMongo2Csv: require('./lib/RssFromMongo2Csv'),
-  ExportFeedSourcesFromMongo: require('./lib/ExportFeedSourcesFromMongo'),
   ExportRss: require('./lib/ExportRss'),
 
   ExportWeixinWatchList: require('./lib/ExportWeixin').exportWeixinWatchList,
