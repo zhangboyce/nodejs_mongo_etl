c8fe1e8c7bdbb060406745757b42e8b856c9d3b6
Boyce <Boyce.zhang@ccegroup.com>
2016-09-07 15:18:02 +0800

    etl

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
 module.exports = {
-  ExportRssProjects: require('./lib/ExportRss').exportRssProjects,
-  ExportRssWatchList: require('./lib/ExportRss').exportRssWatchList,
+  ExportRssProjects: require('./lib/ExportRss').exportProjects,
+  ExportRssWatchList: require('./lib/ExportRss').exportWatchList,
 
-  ExportWeixinWatchList: require('./lib/ExportWeixin').exportWeixinWatchList,
-  ExportWeixinProjects: require('./lib/ExportWeixin').exportWeixinProjects,
+  ExportWeixinWatchList: require('./lib/ExportWeixin').exportWatchList,
+  ExportWeixinProjects: require('./lib/ExportWeixin').exportProjects,
 
-  ExportInstagramWatchList: require('./lib/ExportInstagram').exportInstagramWatchList,
-  ExportInstagramProjects: require('./lib/ExportInstagram').exportInstagramProjects
+  ExportInstagramWatchList: require('./lib/ExportInstagram').exportWatchList,
+  ExportInstagramProjects: require('./lib/ExportInstagram').exportProjects,
+
+  ExportFacebookWatchList: require('./lib/ExportFacebook').exportWatchList,
+  ExportFacebookProjects: require('./lib/ExportFacebook').exportProjects,
+
+  ExportTwitterWatchList: require('./lib/ExportTwitter').exportWatchList,
+  ExportTwitterProjects: require('./lib/ExportTwitter').exportProjects,
+
+  ImportProject2ES: require('./lib/Import2ES').importProject,
+  ImportFeedSource2ES: require('./lib/Import2ES').importFeedSource
 };
