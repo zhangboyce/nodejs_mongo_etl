3a5a5e58f00a2d7d14e117c211c84445913d53f9
Boyce <Boyce.zhang@ccegroup.com>
2016-09-22 12:00:20 +0800

    update mongo config

--- Config.js
+++ Config.js
@@ -1,19 +1,25 @@
 'use strict';
 
 exports.mongo_config = {
-    export: {
-        host: '100.99.164.67',
-        port: '3717',
-        username: 'h1iqeel8fr',
-        password: 'tb53xiggb9'
-    },
-    import: {
-        host: '192.168.100.83',
-        port: '27017',
-    }
+    export_content_url: 'mongodb://contentpool:contentpool@' +
+    'dds-bp17568c88318c341.mongodb.rds.aliyuncs.com:3717,' +
+    'dds-bp17568c88318c342.mongodb.rds.aliyuncs.com:3717/contentpool?' +
+    'replicaSet=mgset-1471849',
+
+    export_raw_url: 'mongodb://raw:raw@' +
+    'dds-bp17568c88318c341.mongodb.rds.aliyuncs.com:3717,' +
+    'dds-bp17568c88318c342.mongodb.rds.aliyuncs.com:3717/raw?' +
+    'replicaSet=mgset-1471849',
+
+    import_url: 'mongodb://192.168.100.83:27017/boom'
 };
 
-exports.es_config = {
-    host: '192.168.100.83',
-    port: '9200'
+exports.es_etl_config = {
+    es: {
+        host: '192.168.100.83:9200',
+        log: 'info'
+    },
+    mongo: {
+        url: 'mongodb://192.168.100.83:27017/boom'
+    }
 };
