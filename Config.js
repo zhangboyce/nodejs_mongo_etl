8747e93a97f505d2d5056d87098224523aff9394
Boyce <Boyce.zhang@ccegroup.com>
2016-09-22 16:04:27 +0800

    update

--- Config.js
+++ Config.js
@@ -1,25 +1,19 @@
 'use strict';
 
 exports.mongo_config = {
-    export_content_url: 'mongodb://contentpool:contentpool@' +
-    'dds-bp17568c88318c341.mongodb.rds.aliyuncs.com:3717,' +
-    'dds-bp17568c88318c342.mongodb.rds.aliyuncs.com:3717/contentpool?' +
-    'replicaSet=mgset-1471849',
+    export_content_url: '',
 
-    export_raw_url: 'mongodb://raw:raw@' +
-    'dds-bp17568c88318c341.mongodb.rds.aliyuncs.com:3717,' +
-    'dds-bp17568c88318c342.mongodb.rds.aliyuncs.com:3717/raw?' +
-    'replicaSet=mgset-1471849',
+    export_raw_url: '',
 
-    import_url: 'mongodb://192.168.100.83:27017/boom'
+    import_url: ''
 };
 
 exports.es_etl_config = {
     es: {
-        host: '192.168.100.83:9200',
+        host: '',
         log: 'info'
     },
     mongo: {
-        url: 'mongodb://192.168.100.83:27017/boom'
+        url: ''
     }
 };
