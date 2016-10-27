8081171678aaa579883e4c344efb138041305504
Boyce <Boyce.zhang@ccegroup.com>
2016-10-27 18:37:35 +0800

    log

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-run -d feedsource -h 48 -i mongo -r 10 -c '0 1 * * *' > ~/log/momgo_feedsource.txt &
-run -d project -h 48 -i mongo -r 10 -c '0 2 * * *' > ~/log/momgo_project.txt &
+run -d feedsource -h 48 -i mongo -r 100 -c '0 1 * * *' > ~/log/momgo_feedsource.txt &
+run -d project -h 48 -i mongo -r 100 -c '0 2 * * *' > ~/log/momgo_project.txt &
 
-run -d feedsource -h 48 -i es -r 10 -c '0 3 * * *' > ~/log/es_feedsource.txt &
-run -d project -h 48 -i es -r 10 -c '0 4 * * *' > ~/log/es_project.txt &
+run -d feedsource -h 48 -i es -r 100 -c '0 6 * * *' > ~/log/es_feedsource.txt &
+run -d project -h 48 -i es -r 100 -c '0 7 * * *' > ~/log/es_project.txt &
